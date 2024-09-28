83386dda1788e0b35915d61f7d182016672ab1e5
ShawnZhu03 <108154998+ShawnZhu03@users.noreply.github.com>
2024-09-28 13:46:11 -0500

    fixed sticky header

--- scripts/ts/dynamic-dom/header.ts
+++ scripts/ts/dynamic-dom/header.ts
@@ -2,6 +2,7 @@ export function stickyHeader() {
     
     window.addEventListener('scroll', (e) => {
         const navbar = document.getElementById("main-menu-container");
+        console.log("sticky");
         if (window.scrollY > 100) {
             navbar?.classList.add('sticky');
         } else {
